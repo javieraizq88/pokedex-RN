7e1055d13cd7ef1e424c0a290a1a9db1d5d525fc
javiera izquierdo <jizquierdo.3it@bancochile.cl>
2024-07-25 15:33:57 -0400

    bf home screen funcionando

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,11 +1,12 @@
-import { Text, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
 import { ActivityIndicator, Button } from "react-native-paper"
 import { getPokemons } from "../../../actions/pokemons";
 import { useQuery } from "@tanstack/react-query";
+import { PokeballBg } from "../../components/ui/PokeballBg";
 
 export const HomeScreen = () => {
 
-    const { isLoading, data } = useQuery({
+    const { isLoading, data = [] } = useQuery({
         queryKey: ["pokemons"],
         queryFn: () => getPokemons(0),
         staleTime: 1000 * 60 * 65, // TODO guarda la data en cache despues de 65 min
@@ -13,21 +14,16 @@ export const HomeScreen = () => {
 
     return (
         <View>
-            <Text>HomeScreen</Text>
-
-            {
-                isLoading ? (
-                    <ActivityIndicator />
-                ) : (
-                    <Button
-                        mode="contained"
-                        onPress={() => console.log("pressed")}
-                    >
-                        Press me
-                    </Button>
-                )
-            }
+            <PokeballBg style={styles.imgPosition} />
 
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    imgPosition: {
+        position: "absolute",
+        top: -100,
+        right: -100
+    }
+})
